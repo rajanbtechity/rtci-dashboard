f0b119632d6eaa5ba851a8711d0557a0d005ee0f
rajanbtechity <rajanbtechity@gmail.com>
2017-08-15 18:12:05 +0530

    Update JiraIR.java

--- src/zephyrAPICall/JiraIR.java
+++ src/zephyrAPICall/JiraIR.java
@@ -1,79 +1,133 @@
 package zephyrAPICall;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ExecuteSOAPTestCase {
-	
-	public static String executeSOAPUI(String loc, String filename)
-	{
-		try
-		{
-		String[] split;
-		String suit_name="";
-		String test_case="";
-		String file_cycle=loc+"/"+filename+".txt";
-		File file = new File(file_cycle);
-		File soapui_exec_file=new File("C:/Users/xyz/Desktop/SOAPUI/trigger_soapui/execute_soapui.bat");
-		FileOutputStream soapui_exec_write=new FileOutputStream(soapui_exec_file);
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(soapui_exec_write));
-		FileReader cycle_fileReader = new FileReader(file);
-		bw.write("cd C:/program files/SmartBear/SoapUI-5.0.0/bin");
-		bw.newLine();
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-	
-					BufferedReader bufferedReader = new BufferedReader(cycle_fileReader);
-		
-					String line;
-					while ((line = bufferedReader.readLine()) != null) {
-						split=line.split(",");
-						suit_name=split[0];
-						test_case=split[1];
-						System.out.println(suit_name+"#"+test_case);
-						bw.write("CALL testrunner.bat -s"+suit_name+" -c\""+test_case+"\" -I -r -a -f C:/Users/xyz/Desktop/SOAPUI/report_txt C:/Users/v536139/Desktop/SOAPUI/RTCITestAutomation_Aug16_v3(local)-soapui-project.xml");
-						bw.newLine();
-						suit_name="";test_case="";
-					}	
-					bw.write("exit");
-					
-					bw.close();
-					cycle_fileReader.close();
-					executeScript();
-					return "SOAPUI Execution Completed";
 
-		}
-		catch(Exception e)
-		{
-		e.printStackTrace();
-		return "Error4";
-		}
-		
-	}
+
+public class JiraIR {
 	
-	
-	
-	public static void executeScript() throws IOException
-	{
-		 Process proc=Runtime.getRuntime().exec("C:/Users/xyz/Desktop/SOAPUI/trigger_soapui/execute_soapui.bat");
-		 BufferedReader read = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+ 
+    /** URLS */
+    private static final String BASE_URL = "https://onejira.verizon.com";
+    private String ZAPI_URL="";
+    
+    JiraIR(String resource_url)
+    {
+    	ZAPI_URL=BASE_URL + resource_url;
+    }
+    
+    
+ 
+    /** JIRA credentials: format "username:password" or "" for none. */
+    private static final String CREDENTIALS = "vzid:password";
+
+
+    private static JSONObject sendRequest(final String url, final JSONObject obj,final String method) throws IOException {
+        final HttpURLConnection httpCon = createHttpCon(url, method);
+        final StringBuffer result = new StringBuffer();
+ 
+        if (null != obj) {
+            final OutputStreamWriter out = new OutputStreamWriter(httpCon.getOutputStream());
+            out.write(obj.toString());
+            out.close();
+        }
+ 
+        final BufferedReader rd =
+                new BufferedReader(new InputStreamReader(httpCon.getInputStream()));
+        
+        String line = "";
+        while (null != (line = rd.readLine())) {
+            result.append(line);
+        }
+        
        
-           new InputStreamReader(proc.getInputStream());
-           String line1 = null;
-           while ((line1 = read.readLine()) != null) {System.out.println(line1); }
-		}
+			try {
+				return new JSONObject(result.toString());
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return obj;
+			}
+        
+    }
+ 
+   
+    private static JSONObject post(final String url, final JSONObject obj) throws IOException, JSONException {
+        return sendRequest(url, obj, "POST");
+    }
+ 
+
+   
+    private static HttpURLConnection createHttpCon(final String url, final String method)
+            throws IOException {
+        final HttpURLConnection httpCon;
+
+            httpCon = (HttpURLConnection) new URL(url).openConnection();
+            
+        httpCon.setDoInput(true);
+        httpCon.setDoOutput(true);
+        httpCon.setRequestMethod(method);
+ 
+        if (!CREDENTIALS.isEmpty()) {
+            final String encoding = new Base64().encodeToString(CREDENTIALS.getBytes());
+            httpCon.setRequestProperty("Authorization", "Basic " + encoding);
+        }
+ 
+        httpCon.setRequestProperty("Content-type", "application/json");
+ 
+        return httpCon;
+    }
+    
+    
+	public static void main(String[] args) throws IOException, JSONException {
+		// TODO Auto-generated method stub
+		try{
+		String cycle_param="";
+		String cycle_exe_param="";
+		String Jira_ir_param="";
 	
-	
-	public static void main(String[] args)
-	{
-		System.out.println(executeSOAPUI("C:/Users/xyz/Desktop/Jira_Soap/zephyr","PPSHInfoTestSuite"));
-		System.out.println("Rajan");
+			JiraIR jira_ir_obj=new JiraIR("/rest/api/2/issue");
+			Jira_ir_param="{\"fields\": {\"project\": {\"key\": \"EQHV\"},\"issuetype\": {\"id\": \"1\",\"name\": \"Bug\"},\"summary\":\""+
+			"Test Summary"+
+					"\",\"description\":\""+"Test Description"+"\"}}";
+			JSONObject json_jira_param=new JSONObject(Jira_ir_param);
+			System.out.println("url:"+jira_ir_obj.ZAPI_URL+" Param"+json_jira_param);
+			System.out.println(post(jira_ir_obj.ZAPI_URL,json_jira_param));
 		
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		catch(JSONException e)
+		{
+			e.printStackTrace();
+		}
+
 	}
 
 }
